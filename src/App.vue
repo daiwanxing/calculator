b9cbf5a77c4d03cc905585bc27e77e45cad8fd28
daiwanxing <377099119@qq.com>
2022-09-06 22:43:15 +0800

    add normalize.css

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
 <script lang="ts" setup>
 import { ref } from "vue";
 const SubTitle = ref("A Caluculator App!");
+
 </script>
 
 <template>
     <h1>{{ SubTitle }}</h1>
     <h2>Ok! Here We go! ！！！</h2>
 </template>
+
+
+<style>
+    h1 {
+        color: #09f;
+    }
+</style>
